f610f9ef611bbfd719792ef2cf9b92d75f291e7d
Fernando Picoral <fernandopicoral@gmail.com>
2019-12-18 02:15:05 -0300

    Added footer

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ const menu = {
         toggle: $("#menu-toggle-a"),
         home: $dom.getMenuBtn("home"),
         projects: $dom.getMenuBtn("projects"),
-        about: $dom.getMenuBtn("about"),
+        skills: $dom.getMenuBtn("skills"),
         contact: $dom.getMenuBtn("contact"),
         other: $dom.getMenuBtn("other")
 
@@ -320,6 +320,43 @@ let projects = {
             }
         }
     }
-
 })();
 
+//Handle footer links
+(function () {
+    let f =  $('footer:first');
+
+   f.children('h1').click(function () {
+        window.scrollTo(0,0);
+    });
+
+    for (let a of f.children('ul').children('a')) {
+        let token = $(a).children('i')[0].classList[1];
+        switch (token) {
+            case 'f':
+                $(a).remove();
+                a.href = "https://www.facebook.com/fernandopicora";
+                break;
+            case 't':
+                $(a).remove();
+                a.href = "https://twitter.com/fe_picoral";
+                break;
+            case 'r':
+                $(a).remove();
+                a.href = "https://www.reddit.com/u/fpicoral";
+                break;
+            case 'l':
+                a.href = "https://www.linkedin.com/in/fernando-picoral-51173a35/";
+                break;
+            case 'm':
+                continue;
+            case 'i':
+                $(a).remove();
+                a.href = "https://www.instagram.com/fernandopicoral/";
+                break;
+
+        }
+    }
+
+
+})();
