216b6910dcd118b9ed700743633fd7ffa98b1f5e
Fernando Picoral <fernandopicoral@colorado.edu>
2021-04-07 11:11:29 -0300

    minor changes to the about section content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -41,8 +41,8 @@ const About = () => {
                 <AboutItem>
                     <AboutTitle>Web</AboutTitle>
                     <AboutDescription>
-                        HTML, CSS, React, Redux, Next, Node, Bootstrap, jQuery
-                        and Styled Components
+                        HTML, CSS, React, Redux, Next, Node, Express, Bootstrap,
+                        jQuery and Styled Components
                     </AboutDescription>
                 </AboutItem>
                 <AboutItem>
@@ -52,14 +52,15 @@ const About = () => {
                 <AboutItem>
                     <AboutTitle>Other</AboutTitle>
                     <AboutDescription>
-                        Figma, AdobeXD, Git, GitHub & GitHub Actions
+                        Figma, AdobeXD, Linux, Vercel, Git, GitHub & GitHub
+                        Actions
                     </AboutDescription>
                 </AboutItem>
                 <AboutItem>
                     <AboutTitle>Education</AboutTitle>
                     <AboutDescription>
                         University of Colorado - Boulder (Class 2026)
-                        <br />
+                        <div style={{ marginTop: 10 }} />
                         Computer Science, BS
                     </AboutDescription>
                 </AboutItem>
